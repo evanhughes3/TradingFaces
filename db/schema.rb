60fffbb17e4a759537133afcddd40450bb5122a9
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-27 15:43:51 -0700

    add columns necessary for omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327013651) do
+ActiveRecord::Schema.define(version: 20150327223226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,10 +47,12 @@ ActiveRecord::Schema.define(version: 20150327013651) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "username"
     t.string   "photo_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "provider"
+    t.integer  "uid"
+    t.string   "oauth_token"
   end
 
 end
