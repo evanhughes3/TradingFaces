38717fd8876c4cbf11b9b4736df5f23e15600442
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-27 12:19:23 -0700

    ignore .env file

---
+++ app/controllers/oauth_controller.rb
@@ -0,0 +1,1 @@
+CLIENT_ID='http://localhost:1337/login'
---
+++ app/models/auth.rb
@@ -0,0 +1,18 @@
+class Authentication
+
+  def check_login_state
+  end
+
+  def log_user_in
+  end
+
+  def verify_identity
+  end
+
+  def api_call
+  end
+
+  def log_out
+  end
+
+end
