86a922cb1fde74d21969226394c16706297a5ffc
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-26 22:28:30 -0700

    user and photo specs added for vaidations & associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
   belongs_to :round
+
+  validates :img_url, :user_id, :round_id, presence: true
+  validates :img_url, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :games, foreign_key: 'winner_id'
   has_many :players
   has_many :rounds, through: :players
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
