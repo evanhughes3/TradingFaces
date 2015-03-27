7598c52ffaf68ebdd7e754626735aa08be3d47fe
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-03-27 11:37:22 -0700

    successfully saved base64 data as an image

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,9 +2,9 @@ class PhotosController < ApplicationController
 
 
 	def create
-		new_image = File.new("test_image.png", 'wb')
-		new_image.write(Base64.decode64(params[:image_data]))
-		p new_image
+		data_url = params[:image_data]
+		png = Base64.decode64(data_url['data:image/png;base64,'.length .. -1])
+		new_image = File.open("test_image.png", 'wb') { |f| f.write(png) }
 	end
 
 
