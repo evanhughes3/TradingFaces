e96ff73199799a253e09fd3b6599fdd5f7826876
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-27 15:43:14 -0700

    omniauth setup finalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end
