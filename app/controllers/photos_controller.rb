56d01a8e2b47edc229c2b0a8c13d5142eca3e577
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-03-27 11:59:41 -0700

    able to send image to cloudinary and get response data

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,10 @@ class PhotosController < ApplicationController
 	def create
 		data_url = params[:image_data]
 		png = Base64.decode64(data_url['data:image/png;base64,'.length .. -1])
-		new_image = File.open("test_image.png", 'wb') { |f| f.write(png) }
+		new_image = File.open("test_image.png", 'wb')
+		new_image.write(png)
+		response = Cloudinary::Uploader.upload(new_image.path)
+		photo = Photo.create(img_url: response["secure_url"])
 	end
 
 
