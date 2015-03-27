120f13c2e9e1e4f194436a2ac29155fd0c906ea5
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-27 16:44:59 -0700

    starting javascript & ajax for fb auth

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,15 +1,8 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '716047925174912',
-    xfbml      : true,
-    version    : 'v2.3'
-  });
-};
+$(document).ready(function() {
 
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+  $('#fb_login').on('click', function(event){
+    event.preventDefault();
+    console.log('Andrew and Cari are bosses');
+  })
+
+});
