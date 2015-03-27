f6a9fed03689a70d246bbc751667bb58106c4dbf
Evan Hughes <evanhughes3@gmail.com>
2015-03-26 17:15:40 -0700

    setting up rails-api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+TradingFaces::Application.config.secret_token = '85f548a0c008f402088724fe8b81a3e63dd4505c8f178e81e31ba2eb8420b640d937369f4498d5de035450bc9a690a84efb693cb3782794a0c02b696f1dffd3a'
