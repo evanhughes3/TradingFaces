aa8226987b658cd65fc60b565a79b86ddcc6830c
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-03-27 18:16:17 -0700

    create helper files for games, photos, and rounds

---
+++ app/helpers/rounds_helper.rb
@@ -0,0 +1,12 @@
+module RoundsHelper
+
+  def compare(id)
+		round = Round.find(id)
+		photos = round.photos.pluck('face_id')
+		url = "https://apius.faceplusplus.com/v2/recognition/compare?api_secret=#{ENV['FACE_PLUS_SECRET']}&api_key=#{ENV['FACE_PLUS_KEY']}&face_id2=#{photos[1]}&face_id1=#{photos[0]}"
+		response = HTTParty.get(url)
+		rating = response['similarity'].round
+		return rating
+ end
+
+end
