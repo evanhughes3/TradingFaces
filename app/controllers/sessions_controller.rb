0662040b2727183fbd1268bf7281ab726955a742
Evan Hughes <evanhughes3@gmail.com>
2015-03-28 15:53:12 -0700

    redirect to user#index instead of render json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
-    # redirect_to root_url
-    render :json => { user: user }
+    # render :json => { user: user }
+    redirect_to user_index_path
   end
 
   def destroy
