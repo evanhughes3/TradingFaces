5cbfe6c8358800173c1355fb827a350c9a97ea21
Evan Hughes <evanhughes3@gmail.com>
2015-03-28 22:09:01 -0700

    found one bug, but can't access photos now?

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
 
-  validates :user_id, :round_id, presence: true
+  validates :user_id, presence: true
   # validate :unique_user_round_combination
 
 
