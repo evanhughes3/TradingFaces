38b11aed96ea5c5515e00404a9877195a0ce06b3
Andrew Schnieder <andrewschnieder@gmail.com>
2015-03-28 17:21:14 -0700

    landing.js and css

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -19,3 +19,37 @@ $(document).ready(function() {
   // });
 
 });
+
+
+//this is Andrew's initial ajax for the facebook login button on the landing page
+
+// $(document).ready(function(){
+//   $('#facebook_button').on('click', function(event){
+//     event.preventDefault();
+
+//     $.ajax({
+//       url: '/',
+//       type: 'default GET (Other values: POST)',
+//       dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+//       data: {param1: 'value1'},
+//     })
+//     .done(function() {
+//       console.log("success");
+//     })
+//     .fail(function() {
+//       console.log("error");
+//     })
+//     .always(function() {
+//       console.log("complete");
+//     });
+
+
+//   })
+
+
+
+
+
+
+
+});
