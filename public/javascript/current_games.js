6ebf1e0576ddb615368a284721d057c21cabc931
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-03-29 15:54:27 -0700

    fix method to return only current games

--- public/javascript/current_games.js
+++ public/javascript/current_games.js
@@ -1,22 +1,12 @@
 function getCurrentGames (argument) {
-	var currentGameData = {};
 
-	var ajaxUser = $.ajax({
-	  url: '/users/show',
-	});
-
-	ajaxUser.done(function (userData) {
-	  var userId = userData.id;
-	  currentGameData.user = userData;
 	  var ajaxCurrentGames = $.ajax({
-	  	url: '/users/' + userId + '/games',
+	  	url: '/games/current_games',
 	  });
 
 	  ajaxCurrentGames.done(function (gameData) {
-	  	currentGameData.games = gameData;
-	  	console.log(currentGameData);
+	  	console.log(gameData);
 
 	  });
 
-	});
 }
