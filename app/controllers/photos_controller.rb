3e0eea74e521c32c4436990b59a66cf2b1ea626f
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-03-30 15:34:48 -0700

    add new round when photo's compare

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,6 +5,9 @@ class PhotosController < ApplicationController
 		photo = Photo.create_new_photo(params[:image_data], round, current_user )
 		if round.photos.length == 2
 			round.compare_photos
+			game = round.game
+			opponent_id = game.players.pluck('user_id').select { |id| id != current_user.id }
+			game.rounds.create(responder_id: opponent_id[0] )
 			render json: {round: round, finished_round: true}
 		else
 			render json: {photo: photo, finished_round: false}
