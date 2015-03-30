d26516ee1cab22254ae57dc9ed1e34882844515e
Andrew Schnieder <andrewschnieder@gmail.com>
2015-03-30 15:07:00 -0700

    add lightbox for friends list

--- public/javascript/new_game.js
+++ public/javascript/new_game.js
@@ -29,7 +29,7 @@ var renderUsers = function(data) {
   var context = { friends: data }
   var html = $('#friends_to_challenge_template').html();
   var friendsTemplate = Handlebars.compile(html);
-  $('#friends-overlay').append(friendsTemplate(context));
+  $('.start-game').append(friendsTemplate(context));
 }
 
 var startNewGameListener = function() {
