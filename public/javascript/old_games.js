64391542df9b8d74cd831eb8bcd86bf4afd0bf29
Evan Hughes <evanhughes3@gmail.com>
2015-03-29 17:57:44 -0700

    zacs refactor

--- public/javascript/old_games.js
+++ public/javascript/old_games.js
@@ -1,17 +1,30 @@
-$(document).ready(function() {
-  $('#old-games').click(function(event) {
-    event.preventDefault()
-    $.ajax({
-      url: '/games/finished_games',
-      type: 'GET',
-    })
-    .done(function(response) {
-      console.log("success old games");
-      parseResponse(response)
-    })
-    .fail(function() {
-      console.log("error old games");
-    })
+// $('#old-games').click(function(event) {
+//   event.preventDefault()
+//   $.ajax({
+//     url: '/games/finished_games',
+//     type: 'GET',
+//   })
+//   .done(function(response) {
+//     console.log("success old games");
+//     parseResponse(response)
+//   })
+//   .fail(function() {
+//     console.log("error old games");
+//   })
 
-  });
-});
+// });
+
+
+function getOldGames (event) {
+  event.preventDefault();
+  $.ajax({
+    url: '/games/finished_games'
+  })
+  .done(function(gameData) {
+    console.log("success old games");
+    console.log(gameData)
+  })
+  .fail(function() {
+    console.log("error old games");
+  })
+}
