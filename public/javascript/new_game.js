3889440c258bc5432043a254baf6fa939a57a766
Evan Hughes <evanhughes3@gmail.com>
2015-03-30 15:57:42 -0700

    fixed multiple games bug

--- public/javascript/new_game.js
+++ public/javascript/new_game.js
@@ -19,6 +19,7 @@ var getAllFriends = function() {
 }
 
 var friendsOverlay = function () {
+  $(".start-game").empty();
   el = document.getElementById("friends-overlay");
   el.style.visibility = (el.style.visibility == "visible") ?
   "hidden" : "visible";
