b53a54b54fe5292c28725ceb4578b5c06683aad7
Evan Hughes <evanhughes3@gmail.com>
2015-03-30 15:23:22 -0700

    working on spiral

--- public/javascript/spiral.js
+++ public/javascript/spiral.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-  $('#save-photo').click(function(event) {
-  console.log("ahahah")
-    showSpiral();
+  $('.save-photo').click(function(event) {
+    checkMark();
   });
-});
+})
+
+
+
 
 var showSpiral = function() {
-  $('loading-gif').addClass("fa fa-cog fa-spin")
+  $('#loading-gif').addClass("fa fa-spinner fa-pulse fa-3x")
 }
 
 var hideSpiral = function() {
+  $('#loading-gif').removeClass("fa fa-spinner fa-pulse fa-3x")
+}
+
+var checkMark = function() {
+  $('#loading-gif').addClass("fa fa-check fa-3x").delay(1000).removeClass("fa fa-check fa-3x")
 }
