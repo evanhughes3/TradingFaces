2e17ba6e07795726087706288db98f3199df46c9
Evan Hughes <evanhughes3@gmail.com>
2015-03-31 10:49:46 -0700

    fix method to declare winners

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,28 +9,29 @@ class Game < ActiveRecord::Base
 
   def declare_winner
       users_ratings = self.rounds.pluck(:rating, :responder_id)
-      # [[94, 2], [99, 2]]
+
       user1_score = users_ratings[0][0]
       user2_score = users_ratings[1][0]
 
-      user1 = users_ratings[0][1]
-      user2 = users_ratings[1][1]
+      responder1 = users_ratings[0][1]
+      responder2 = users_ratings[1][1]
+
 
       if user1_score > user2_score
-        player1 = Player.where(user_id: user1, game_id: self.id).first
+        player1 = Player.where(user_id: responder1, game_id: self.id).first
         player1.winner = true
         player1.save!
       else
-        player2 = Player.where(user_id: user2, game_id: self.id).first
+        player2 = Player.where(user_id: responder2, game_id: self.id).first
         player2.winner = true
         player2.save!
       end
   end
 
-  def winner
-    winner = self.players.select { |player| player.winner == true }
-    return winner.first
-  end
+  # def winner
+  #   winner = self.players.select { |player| player.winner == true }
+  #   return winner.first
+  # end
 
 
 
