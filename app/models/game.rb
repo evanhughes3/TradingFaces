898f31797202004ceaf81a06aaff8fa53753706b
Evan Hughes <evanhughes3@gmail.com>
2015-03-31 08:08:21 -0700

    determine winner

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,27 +7,32 @@ class Game < ActiveRecord::Base
   	self.players.pluck('winner').include?(true) ? true : false
   end
 
-  def return_winner
-    users_ratings = self.rounds.pluck(:rating, :responder_id)
-    # [[94, 2], [99, 2]]
-    user1_score = users_ratings[0][0]
-    user2_score = users_ratings[1][0]
+  def declare_winner
+    if self.over?
+      users_ratings = self.rounds.pluck(:rating, :responder_id)
+      # [[94, 2], [99, 2]]
+      user1_score = users_ratings[0][0]
+      user2_score = users_ratings[1][0]
 
-    user1 = users_ratings[0][1]
-    user2 = users_ratings[1][1]
+      user1 = users_ratings[0][1]
+      user2 = users_ratings[1][1]
 
-    if user1_score > user2_score
-      player1 = Player.where(user_id: user1, game_id: self.id).first
-      player1.winner = true
-      player1.save
-      return player1
+      if user1_score > user2_score
+        player1 = Player.where(user_id: user1, game_id: self.id).first
+        player1.winner = true
+        player1.save
+        p 'PLAYER 1 WINS'
+        return player1
+      else
+        player2 = Player.where(user_id: user2, game_id: self.id).first
+        player2.winner = true
+        player2.save
+        p 'PLAYER 2 WINS'
+        return player2
+      end
     else
-      player2 = Player.where(user_id: user2, game_id: self.id).first
-      player2.winner = true
-      player2.save
-      return player2
+      p "game is not over yet"
     end
-
   end
 
 
