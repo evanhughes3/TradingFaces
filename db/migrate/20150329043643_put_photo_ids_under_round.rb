cf6a8f7e9331c839f149b2e638e33ca4990cf069
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-03-31 17:45:05 -0700

    removed old migrations that were erroneously pushed to master

--- db/migrate/20150329043643_put_photo_ids_under_round.rb
+++
@@ -1,6 +0,0 @@
-class PutPhotoIdsUnderRound < ActiveRecord::Migration
-  def change
-    add_column :rounds, :first_photo_id, :integer
-    add_column :rounds, :second_photo_id, :integer
-  end
-end
--- db/migrate/20150329043734_remove_round_id_from_photos.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRoundIdFromPhotos < ActiveRecord::Migration
-  def change
-    remove_column :photos, :round_id, :integer
-  end
-end
