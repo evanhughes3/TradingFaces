0cbdacd0389d9f6d70b1f4d5c3d51cd57b2e8710
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-04-01 12:34:41 -0700

    push photos to cloud and use cloud link for image source

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -11,27 +11,27 @@ function getUser () {
 
 function toggleNavbarFaces() {
   $('#new-game').children().hover(function(){
-    $(this).attr("src", "../images/wink_icon.png");
+    $(this).attr("src", "https://res.cloudinary.com/hvytc53yh/image/upload/v1427915724/wink_icon_s3oyss.png");
     }, function() {
-    $(this).attr("src", "../images/smile_icon.png");
+    $(this).attr("src", "https://res.cloudinary.com/hvytc53yh/image/upload/v1427915724/smile_icon_knje1j.png");
   })
 
   $('#current-games').children().hover(function(){
-    $(this).attr("src", "../images/smile_icon.png");
+    $(this).attr("src", "https://res.cloudinary.com/hvytc53yh/image/upload/v1427915724/tongue_icon_y3i0t3.png");
     }, function() {
-    $(this).attr("src", "../images/love_icon.png");
+    $(this).attr("src", "https://res.cloudinary.com/hvytc53yh/image/upload/v1427915723/love_icon_idmzkn.png");
   })
 
   $('#old-games').children().hover(function(){
-    $(this).attr("src", "../images/laugh_icon.png");
+    $(this).attr("src", "https://res.cloudinary.com/hvytc53yh/image/upload/v1427915723/laugh_icon_ik6was.png");
     }, function() {
-    $(this).attr("src", "../images/whistle_icon.png");
+    $(this).attr("src", "https://res.cloudinary.com/hvytc53yh/image/upload/v1427915724/whistle_icon_egojo8.png");
   })
 
   $('#logout').children().hover(function(){
-    $(this).attr("src", "../images/angry_icon.png");
+    $(this).attr("src", "https://res.cloudinary.com/hvytc53yh/image/upload/v1427915723/angry_icon_evxunp.png");
   }, function() {
-    $(this).attr("src", "../images/wink_icon.png");
+    $(this).attr("src", "https://res.cloudinary.com/hvytc53yh/image/upload/v1427915724/wink_icon_s3oyss.png");
   })
 }
 
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
  $('#facebook_button').hover(function() {
-   $('#landing-body').css('background-image', 'url(../images/tongue_icon_centered.png)')
+   $('#landing-body').css('background-image', 'url(https://res.cloudinary.com/hvytc53yh/image/upload/v1427915724/tongue_icon_centered_mrfxtt.png)')
  }, function() {
-   $('#landing-body').css('background-image', 'url(../images/wink_icon.png)')
+   $('#landing-body').css('background-image', 'url(https://res.cloudinary.com/hvytc53yh/image/upload/v1427915724/wink_icon_s3oyss.png)')
  });
 });
